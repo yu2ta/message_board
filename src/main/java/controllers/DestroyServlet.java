e91c267e8a8c97eb4acbb5be2bb839e276cecee6
yuta.name <yuta_matsuo@r.recruit.co.jp>
2022-01-17 18:40:35 +0900

    DestroyServletを追加

--- src/main/java/controllers/DestroyServlet.java
+++ src/main/java/controllers/DestroyServlet.java
@@ -1,7 +1,6 @@
 package controllers;
 
 import java.io.IOException;
-import java.sql.Timestamp;
 
 import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
@@ -13,11 +12,11 @@ import javax.servlet.http.HttpServletResponse;
 import models.Message;
 import utils.DBUtil;
 
-@WebServlet("/update")
-public class UpdateServlet extends HttpServlet {
+@WebServlet("/destroy")
+public class DestroyServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
-    public UpdateServlet() {
+    public DestroyServlet() {
         super();
     }
 
@@ -27,23 +26,21 @@ public class UpdateServlet extends HttpServlet {
         if (_token != null && _token.equals(request.getSession().getId())) {
             EntityManager em = DBUtil.createEntityManager();
 
-            //セッションスコープからメッセージのIDを取得してDBから取得
+            // セッションスコープからメッセージのIDを取得して
+            // 該当のIDのメッセージ1件のみをデータベースから取得
             Message m = em.find(Message.class, (Integer) (request.getSession().getAttribute("message_id")));
 
-            m.setTitle(request.getParameter("title"));
-            m.setContent(request.getParameter("content"));
-
-            Timestamp currentTime = new Timestamp(System.currentTimeMillis());
-            m.setUpdated_at(currentTime); // 更新日時のみ上書き
-
-            //データベースを更新
             em.getTransaction().begin();
+            em.remove(m); // データ削除
             em.getTransaction().commit();
             em.close();
 
+            // セッションスコープ上の不要になったデータを削除
             request.getSession().removeAttribute("message_id");
-            response.sendRedirect(request.getContextPath() + "/index");
 
+            // indexページへリダイレクト
+            response.sendRedirect(request.getContextPath() + "/index");
         }
     }
+
 }
